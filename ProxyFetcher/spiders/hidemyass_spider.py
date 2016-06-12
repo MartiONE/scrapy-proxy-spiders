2f86673e2e1308eb932ee16f4b1ca25a4d5c0066
unknown <alej.marti@gmail.com>
2016-06-13 01:37:50 +0200

    Test spider

---
+++ ProxyFetcher/spiders/hidemyass_spider.py
@@ -0,0 +1,31 @@
+import scrapy
+from ProxyFetcher.items import ProxyfetcherItem
+
+# Spider for the forum of the website hidemyass.com
+
+class HideMyAssProxySpider(scrapy.Spider):
+    
+        name = "hidemyass"
+        allowed_domains = ["proxylist.hidemyass.com"]
+        start_urls = [
+            "http://proxylist.hidemyass.com/search-1304566#listable"
+        ]    
+        
+def parse(self, response):
+        for i in response.xpath("//tbody/tr"):
+                # Getting the styles
+                style = i.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
+                # Filter the ones that have the display property
+                style = [re.match("\.(\w+){display:none}", x) for x in style]
+                # Refine the array
+                style = [x.group(1) for x in style if x]
+                
+                result = ""
+                for i in response.xpath("//tbody/tr")[0].xpath("td")[1].xpath("span/*|span/text()")[2:]:
+                        if (("class" in i.extract()) & (style[0] not in i.extract())) | ("display: inline" in i.extract()) | ("<" not in i.extract()):
+                                result += i.xpath("text()").extract()[0] if i.xpath("text()").extract() else i.extract().strip()
+                print(result)                
+                yield result
+
+                
+               
