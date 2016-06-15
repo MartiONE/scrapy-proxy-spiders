a6168e95a1b789cab6889e1b19e11c57faa9d34a
unknown <alej.marti@gmail.com>
2016-06-16 00:58:57 +0200

    Import error

--- ProxyFetcher/cronscripts/postgrestoredis.py
+++ ProxyFetcher/cronscripts/postgrestoredis.py
@@ -6,7 +6,6 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
-from sqlalchemy.orm.esc import ObjectDeletedError
 
 '''
 To-do list:
