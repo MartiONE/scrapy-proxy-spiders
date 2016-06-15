8a4f2f525f747ecf7158a565d06b23422f794fbd
MartiONE <martione@mt1shop.com>
2016-06-15 17:52:57 +0200

    Comments and first object deleted error handle

--- ProxyFetcher/cronscripts/postgrestoredis.py
+++ ProxyFetcher/cronscripts/postgrestoredis.py
@@ -7,8 +7,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
 
+'''
+To-do list:
+-Appropiate exception handling
+-Logging
+-Settings fixed, maybe from another file
+-Maybe class inheritance
+'''
+
 DeclarativeBase = declarative_base()
 
+# Settings for the postgres database
 settings = {'drivername': 'postgres',
  'host': 'ec2-54-247-185-241.eu-west-1.compute.amazonaws.com',
  'port': '5432',
@@ -18,6 +27,7 @@ settings = {'drivername': 'postgres',
 
 
 class Proxies(DeclarativeBase):
+    '''Class for the database'''
     __tablename__ = "proxies"
     full_address = Column("full_address",String, primary_key=True)
     ip = Column("ip", String)
@@ -26,12 +36,15 @@ class Proxies(DeclarativeBase):
     con_type = Column("con_type",String)
     response_time = Column("response_time", Float)
 
+# Engine and session creation
 engine = create_engine(URL(**settings))
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Redis session opener
 r = redis.StrictRedis(host="redis-16451.c3.eu-west-1-1.ec2.cloud.redislabs.com", port=16451, password="javicp90")
 
+# Iteration for the query results
 for proxy in session.query(Proxies).filter(Proxies.ip != None).all():
     try:
         judge = requests.get("http://judge.live-proxy.net/index.php", 
@@ -42,6 +55,10 @@ for proxy in session.query(Proxies).filter(Proxies.ip != None).all():
             r.setex(proxy.full_address, 120, proxy.con_type)
     # Exception handling
     except Exception as e:
-        session.delete(proxy)
-        session.commit()
-        print("Bad proxy, deleted "+proxy.full_address)
+        # Deletion from the database
+        try:
+            session.delete(proxy)
+            session.commit()
+            print("Bad proxy, deleted "+proxy.full_address)
+        except sqlalchemy.orm.exc.ObjectDeletedError as e:
+            print("Duplicated key")
