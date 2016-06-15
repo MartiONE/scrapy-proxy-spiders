503aaeeb97043496499124772e08eb60b66e5c2e
unknown <alej.marti@gmail.com>
2016-06-16 00:56:36 +0200

    Exception handling basic

--- ProxyFetcher/cronscripts/postgrestoredis.py
+++ ProxyFetcher/cronscripts/postgrestoredis.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
+from sqlalchemy.orm.esc import ObjectDeletedError
 
 '''
 To-do list:
@@ -47,24 +48,27 @@ r = redis.StrictRedis(host="redis-16451.c3.eu-west-1-1.ec2.cloud.redislabs.com",
 # Iteration for the query results
 for proxy in session.query(Proxies).filter(Proxies.ip != None).all():
     # Socks 4 and 5 not supported for now
-    if proxy.con_type in ["http", "https"]:
-        try:
-            judge = requests.get("http://judge.live-proxy.net/index.php", 
-                                 proxies = {"http" : proxy.full_address}, 
-                                 timeout = 3)
-            # Only store the item if the judge makes a correct answer
-            if judge.status_code == 200:
-                r.setex(proxy.full_address, 120, proxy.con_type)
-        # Exception handling
-        except Exception as e:
-            # Deletion from the database
+    try:
+        if proxy.con_type in ["http", "https"]:
             try:
-                session.delete(proxy)
-                session.commit()
-                print("Bad proxy, deleted "+proxy.full_address)
-            except sqlalchemy.orm.exc.ObjectDeletedError as e:
-                print("Duplicated key")
-    else:
-        print("Not http/https, deleted"+proxy.con_type)
-        session.delete(proxy)
-        session.commit()        
+                judge = requests.get("http://judge.live-proxy.net/index.php", 
+                                     proxies = {"http" : proxy.full_address}, 
+                                     timeout = 3)
+                # Only store the item if the judge makes a correct answer
+                if judge.status_code == 200:
+                    r.setex(proxy.full_address, 120, proxy.con_type)
+            # Exception handling
+            except Exception as e:
+                # Deletion from the database
+                try:
+                    session.delete(proxy)
+                    session.commit()
+                    print("Bad proxy, deleted "+proxy.full_address)
+                except sqlalchemy.orm.exc.ObjectDeletedError as e:
+                    print("Duplicated key")
+        else:
+            print("Not http/https, deleted"+proxy.con_type)
+            session.delete(proxy)
+            session.commit()
+    except Exception as e:
+        print("Exception most likely object error" + e)
