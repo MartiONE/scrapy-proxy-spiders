d246bac0b8a9e1a273bc1591509dbf30a4c8aac0
unknown <alej.marti@gmail.com>
2016-06-17 21:14:34 +0200

    Indentation, starting development of next spider.

--- ProxyFetcher/spiders/hidemyass_spider.py
+++ ProxyFetcher/spiders/hidemyass_spider.py
@@ -1,10 +1,9 @@
 import scrapy
 from ProxyFetcher.items import ProxyfetcherItem
-from scrapy.spiders import CrawlSpider
 
 # Spider for the forum of the website hidemyass.com
 
-class HideMyAssProxySpider(CrawlSpider):
+class HideMyAssProxySpider(scrapy.Spider):
     
         name = "hidemyass"
         allowed_domains = ["proxylist.hidemyass.com"]
@@ -12,24 +11,24 @@ class HideMyAssProxySpider(CrawlSpider):
             "http://proxylist.hidemyass.com/search-1304566"
         ]    
         
-def parse(self, response):
-        for i in response.xpath("//tbody/tr"):
-                self.logger.info(str(i))
-                # Getting the styles
-                style = i.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
-                # Filter the ones that have the display property
-                style = [re.match("\.(\w+){display:none}", x) for x in style]
-                # Refine the array
-                style = [x.group(1) for x in style if x]
-                
-                result = ""
-                for i in response.xpath("//tbody/tr")[0].xpath("td")[1].xpath("span/*|span/text()")[2:]:
-                        if (("class" in i.extract()) & (style[0] not in i.extract())) | ("display: inline" in i.extract()) | ("<" not in i.extract()):
-                                result += i.xpath("text()").extract()[0] if i.xpath("text()").extract() else i.extract().strip()
-                self.logger.info(result) 
-                item = ProxyfetcherItem()
-                item["ip"] = result
-                yield item
+        def parse(self, response):
+                for i in response.xpath("//tbody/tr"):
+                        self.logger.info(str(i))
+                        # Getting the styles
+                        style = i.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
+                        # Filter the ones that have the display property
+                        style = [re.match("\.(\w+){display:none}", x) for x in style]
+                        # Refine the array
+                        style = [x.group(1) for x in style if x]
+                        
+                        result = ""
+                        for i in response.xpath("//tbody/tr")[0].xpath("td")[1].xpath("span/*|span/text()")[2:]:
+                                if (("class" in i.extract()) & (style[0] not in i.extract())) | ("display: inline" in i.extract()) | ("<" not in i.extract()):
+                                        result += i.xpath("text()").extract()[0] if i.xpath("text()").extract() else i.extract().strip()
+                        self.logger.info(result) 
+                        item = ProxyfetcherItem()
+                        item["ip"] = result
+                        yield item
 
                 
                
