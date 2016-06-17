b26e4da3d888dd5549f610866d7b0d754150f679
MartiONE <alej.marti@gmail.com>
2016-06-17 21:40:04 +0000

    Flawed version, got about 90% of the right answers

--- ProxyFetcher/spiders/hidemyass_spider.py
+++ ProxyFetcher/spiders/hidemyass_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from ProxyFetcher.items import ProxyfetcherItem
+import re
 
 # Spider for the forum of the website hidemyass.com
 
@@ -12,23 +13,27 @@ class HideMyAssProxySpider(scrapy.Spider):
         ]    
         
         def parse(self, response):
-                for i in response.xpath("//tbody/tr"):
-                        self.logger.info(str(i))
+                for j in response.xpath("//tbody/tr"):
+                        self.logger.info(str(j))
                         # Getting the styles
-                        style = i.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
+                        style = j.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
                         # Filter the ones that have the display property
                         style = [re.match("\.(\w+){display:none}", x) for x in style]
                         # Refine the array
                         style = [x.group(1) for x in style if x]
                         
                         result = ""
-                        for i in response.xpath("//tbody/tr")[0].xpath("td")[1].xpath("span/*|span/text()")[2:]:
-                                if (("class" in i.extract()) & (style[0] not in i.extract())) | ("display: inline" in i.extract()) | ("<" not in i.extract()):
+                        for i in j.xpath("td")[1].xpath("span/*|span/text()")[2:]:
+                                
+                                if ("display: inline" in i.extract()) | ("<" not in i.extract()):
                                         result += i.xpath("text()").extract()[0] if i.xpath("text()").extract() else i.extract().strip()
+                                elif "class" in i.extract():
+                                        if i.xpath("@class").extract()[0] not in style:
+                                                result += i.xpath("text()").extract()[0]
                         self.logger.info(result) 
                         item = ProxyfetcherItem()
                         item["ip"] = result
-                        yield item
+                        print(result)
 
                 
                
