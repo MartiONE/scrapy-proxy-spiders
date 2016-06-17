dd620a597ab5b5f41bb4bb913c0c940d6f3f54b5
MartiONE <alej.marti@gmail.com>
2016-06-17 23:17:37 +0000

    Working version of hidemyass spider

--- ProxyFetcher/spiders/hidemyass_spider.py
+++ ProxyFetcher/spiders/hidemyass_spider.py
@@ -14,11 +14,11 @@ class HideMyAssProxySpider(scrapy.Spider):
         
         def parse(self, response):
                 for j in response.xpath("//tbody/tr"):
-                        self.logger.info(str(j))
+                        #self.logger.info(str(j))
                         # Getting the styles
                         style = j.xpath("td")[1].xpath("span/style/text()").extract()[0].rsplit()
                         # Filter the ones that have the display property
-                        style = [re.match("\.(\w+){display:none}", x) for x in style]
+                        style = [re.match("\.([^\{]+){display:none}", x) for x in style]
                         # Refine the array
                         style = [x.group(1) for x in style if x]
                         
@@ -29,11 +29,17 @@ class HideMyAssProxySpider(scrapy.Spider):
                                         result += i.xpath("text()").extract()[0] if i.xpath("text()").extract() else i.extract().strip()
                                 elif "class" in i.extract():
                                         if i.xpath("@class").extract()[0] not in style:
+                                                #print(i)
                                                 result += i.xpath("text()").extract()[0]
                         self.logger.info(result) 
                         item = ProxyfetcherItem()
                         item["ip"] = result
-                        print(result)
+                        item["port"] = j.xpath("td[3]/text()").extract()[0].strip()
+                        item["country"] = j.xpath("td[4]/span/text()").extract()[1].strip()
+                        item["con_type"] = j.xpath("td[7]/text()").extract()[0].strip()
+                        item["full_address"] = "{}:{}".format(item["ip"], item["port"])
+                        yield item.status_check(item)
+                        #print(result if re.match("([0-9]{1,3}\.){3}[0-9]{1,3}", result) else [result, j.extract(), style])
 
                 
                
