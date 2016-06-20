0d9d214db90e28c5cb5c529cf7a82c2dc17a25d9
MartiONE <martione@mt1shop.com>
2016-06-20 18:29:39 +0200

    First version of the freeproxylists spider

---
+++ ProxyFetcher/spiders/freeproxylists_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+from ProxyFetcher.items import ProxyfetcherItem
+import re
+
+
+class FreeProxyListsSpider(scrapy.Spider):
+        '''Spider for the forum of the website reeproxylists.net'''
+        
+        name = "freeproxylists"
+        allowed_domains = ["freeproxylists.net"]
+        start_urls = [
+                # With a 50% + uptime filtered
+            "http://www.freeproxylists.net/?c=&pt=&pr=&a%5B%5D=0&a%5B%5D=1&a%5B%5D=2&u=50"
+        ]    
+        
+        def parse(self, response):
+                for row in response.xpath("//table[@class='DataGrid']/tr")[1:]:
+                        item = ProxyfetcherItem()
+                        # This website uses JavaScript for encryption of the IP with UTF-16
+                        # Extract payload for the function
+                        payload = row.xpath("td/script/text()")[0].re('"(.*)"')[0]
+                        # Convert it from UTF-16
+                        payload = "".join([chr(int(x, 16)) for x in payload.split("%")[1:]])
+                        # Extract the IP clean, nice try website.
+                        item["ip"] = re.search("(([0-9]{1,3}\.){3}[0-9]{1,3})", payload).group(0)
+                        item["port"] = row.xpath("td[2]/text()").extract()[0].strip()
+                        item["con_type"] = row.xpath("td[3]/text()").extract()[0].lower().strip()
+                        item["country"] = row.xpath("td[5]/text()").extract()[0].strip()
+                        item["full_address"] = "{}:{}".format(item["ip"], item["port"])
+                        yield item.status_check(item)
