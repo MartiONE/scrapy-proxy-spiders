0c2eec97bbfca2bdfa34561f463ee973a13a1428
MartiONE <martione@mt1shop.com>
2016-06-22 18:20:05 +0200

    comments

--- ProxyFetcher/spiders/freeproxylists_spider.py
+++ ProxyFetcher/spiders/freeproxylists_spider.py
@@ -10,35 +10,39 @@ cfg.read("general.cfg")
 
 
 class FreeProxyListsSpider(scrapy.Spider):
-        '''Spider for the forum of the website reeproxylists.net'''
+        '''
+        Spider for the forum of the website reeproxylists.net
+        
+        WARNING - Not working properly right now, catpcha support has to be added.
+        
+        '''
         
         name = "freeproxylists"
         allowed_domains = ["freeproxylists.net"]
         
         def start_requests(self):
                 req = scrapy.Request("http://www.freeproxylists.net/?c=&pt=&pr=&a%5B%5D=0&a%5B%5D=1&a%5B%5D=2&u=50", self.parse)
+                # Gather a random proxy from our redis server
                 r = redis.StrictRedis(host=cfg["remote_redis"]["host"], 
                                       port=cfg["remote_redis"]["port"], 
                                       password=cfg["remote_redis"]["password"])
                 req.meta['proxy'] = "http://"+r.randomkey().decode("utf-8")
-                req.cookies['userno'] = "20160621-000278"
                 yield req
                 
         def parse(self, response):
-                #print(response.text)
                 for row in response.xpath("//table[@class='DataGrid']/tr")[1:]:
-                        #print(len(row.xpath("td")))
+                        # There are empty rows for publicity purposes, ignore them
                         if len(row.xpath("td")) > 1:
-                        	item = ProxyfetcherItem()
-                        	# This website uses JavaScript for encryption of the IP with UTF-16
-                        	# Extract payload for the function
-                        	payload = row.xpath("td/script/text()")[0].re('"(.*)"')[0]
-                        	# Convert it from UTF-16
-                        	payload = "".join([chr(int(x, 16)) for x in payload.split("%")[1:]])
-                        	# Extract the IP clean, nice try website.
-                        	item["ip"] = re.search("(([0-9]{1,3}\.){3}[0-9]{1,3})", payload).group(0)
-                        	item["port"] = row.xpath("td[2]/text()").extract()[0].strip()
-                        	item["con_type"] = row.xpath("td[3]/text()").extract()[0].lower().strip()
-                        	item["country"] = row.xpath("td[5]/text()").extract()[0].strip()
-                        	item["full_address"] = "{}:{}".format(item["ip"], item["port"])
-                        	yield item.status_check(item)
+                                item = ProxyfetcherItem()
+                                # This website uses JavaScript for encryption of the IP with UTF-16
+                                # Extract payload for the function
+                                payload = row.xpath("td/script/text()")[0].re('"(.*)"')[0]
+                                # Convert it from UTF-16
+                                payload = "".join([chr(int(x, 16)) for x in payload.split("%")[1:]])
+                                # Extract the IP clean, nice try website.
+                                item["ip"] = re.search("(([0-9]{1,3}\.){3}[0-9]{1,3})", payload).group(0)
+                                item["port"] = row.xpath("td[2]/text()").extract()[0].strip()
+                                item["con_type"] = row.xpath("td[3]/text()").extract()[0].lower().strip()
+                                item["country"] = row.xpath("td[5]/text()").extract()[0].strip()
+                                item["full_address"] = "{}:{}".format(item["ip"], item["port"])
+                                yield item.status_check(item)
