8d8026483691ee00d188c916851539eff166167c
MartiONE <martione@mt1shop.com>
2016-06-22 18:55:00 +0200

    First version of the incloak spider

---
+++ ProxyFetcher/spiders/incloak_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+from ProxyFetcher.items import ProxyfetcherItem
+
+class InCloakSpider(scrapy.Spider):
+    
+        name = "incloak"
+        allowed_domains = ["http://incloak.com/"]
+        start_urls = [
+            "http://incloak.com/proxy-list/?maxtime=1000&type=hs#list"
+        ]    
+        
+        def parse(self, response):
+                for row in response.xpath("//table[@class='proxy__t']/tbody/tr"):
+                        item = ProxyfetcherItem()
+                        item["ip"] = row.xpath("td[1]/text()").extract()[0].strip()
+                        item["port"] = row.xpath("td[2]/text()").extract()[0].strip()
+                        item["country"] = row.xpath("td[3]/div/text()").extract()[0].strip()
+                        # Split for the dual mode, getting only http
+                        item["con_type"] = row.xpath("td[5]/text()").extract()[0].split(", ")[0]
+                        item["full_address"] = "{}:{}".format(item["ip"], item["port"])
+                        yield item.status_check(item)
