cc0eff2b526361edcdaf128527d219bb4cb67665
MartiONE <alej.marti@gmail.com>
2016-06-28 18:34:38 +0200

    fix for proxylist con_type

--- ProxyFetcher/spiders/proxylist_spider.py
+++ ProxyFetcher/spiders/proxylist_spider.py
@@ -18,6 +18,7 @@ class ProxyListSpider(scrapy.Spider):
                         item["full_address"] = base64.b64decode(j.xpath("li[@class='proxy']/script/text()")[0].re("Proxy\(\'(.+)\'")[0]).decode("utf-8")
                         item["ip"], item["port"] = item["full_address"].split(":")
                         # Even http classes do not exists they might in the future, so better safe than sorry.-
-                        item["con_type"] = j.xpath("li[@class='http' or @class='https']/text()").extract()[0].strip().lower()
-                        item["country"] = j.xpath("descendant::span[@class='country']/@title").extract()[0].strip()
+                        item["con_type"] = j.xpath("li[@class='http' or @class='https']/text()|li/strong/text()").extract()[0].strip().lower()
+                        # Replace used for the special use case of the &nbsp;
+                        item["country"] = j.xpath("descendant::span[@class='country']/@title").extract()[0].strip().replace("\xa0", " ")
                         yield item.status_check(item)
