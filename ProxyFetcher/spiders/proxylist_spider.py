7269bc8acee5483ea48ae9a1e756279589c0611e
MartiONE <alej.marti@gmail.com>
2016-10-25 10:12:57 +0000

    Small fixes for proxylist, backwards compatibility

--- ProxyFetcher/spiders/proxylist_spider.py
+++ ProxyFetcher/spiders/proxylist_spider.py
@@ -23,10 +23,11 @@ class ProxyListSpider(scrapy.Spider):
                         # Item creation and deployment
                         item = ProxyfetcherItem()
                         # This website use a base64 encoding as payload for a js function
-                        item["full_address"] = base64.b64decode(j.xpath("li[@class='proxy']/script/text()")[0].re("Proxy\(\'(.+)\'")[0]).decode("utf-8")
+                        item["full_address"] = str(base64.b64decode(j.xpath("li[@class='proxy']/script/text()")[0].re("Proxy\(\'(.+)\'")[0]).decode("utf-8"))
                         item["ip"], item["port"] = item["full_address"].split(":")
                         # Even http classes do not exists they might in the future, so better safe than sorry.-
-                        item["con_type"] = j.xpath("li[@class='http' or @class='https']/text()|li/strong/text()").extract()[0].strip().lower()
+                        ctype = j.xpath("li[@class='http' or @class='https']/text()|li/strong/text()").extract()[0].strip().lower()
+                        item["con_type"] = "http" if ctype == "-" else ctype
                         # Replace used for the special use case of the &nbsp;
-                        item["country"] = j.xpath("descendant::span[@class='country']/@title").extract()[0].strip().replace("\xa0", " ")
+                        item["country"] = str(j.xpath("descendant::span[@class='country']/@title").extract()[0].strip().replace(u"\xa0", " "))
                         yield item.status_check(item)
